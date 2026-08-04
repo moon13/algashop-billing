0eae3696b3fa8425534f3c10b88685a6698034fc
Fernanda <fernanda.g.araujo@gmail.com>
2026-08-04 20:54:12 -0300

    commit de validações de values objects

--- src/main/java/com/algaworks/algashop/billing/domain/model/invoice/Address.java
+++ src/main/java/com/algaworks/algashop/billing/domain/model/invoice/Address.java
@@ -1,13 +1,12 @@
 package com.algaworks.algashop.billing.domain.model.invoice;
 
 
+import com.algaworks.algashop.billing.domain.model.FieldValidations;
 import lombok.*;
 
 @Getter
 @Setter(AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
-@Builder
 @EqualsAndHashCode
 public class Address {
 
@@ -19,4 +18,22 @@ public class Address {
      private String state;
      private String zipCode;
 
+     @Builder
+     public Address(String street, String number, String complement,
+                    String neighborhood, String city, String state, String zipCode) {
+          FieldValidations.requiresNonBlank(street);
+          FieldValidations.requiresNonBlank(number);
+
+          FieldValidations.requiresNonBlank(neighborhood);
+          FieldValidations.requiresNonBlank(city);
+          FieldValidations.requiresNonBlank(state);
+          FieldValidations.requiresNonBlank(zipCode);
+          this.street = street;
+          this.number = number;
+          this.complement = complement;
+          this.neighborhood = neighborhood;
+          this.city = city;
+          this.state = state;
+          this.zipCode = zipCode;
+     }
 }
--- src/main/java/com/algaworks/algashop/billing/domain/model/invoice/Payer.java
+++ src/main/java/com/algaworks/algashop/billing/domain/model/invoice/Payer.java
@@ -1,13 +1,14 @@
 package com.algaworks.algashop.billing.domain.model.invoice;
 
 
+import com.algaworks.algashop.billing.domain.model.FieldValidations;
 import lombok.*;
+
+import java.util.Objects;
 
 @Getter
 @Setter(AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
-@Builder
 @EqualsAndHashCode
 public class Payer {
 
@@ -17,4 +18,20 @@ public class Payer {
      private String email;
      private Address address;
 
+
+
+     @Builder
+    public Payer(String fullName, String document, String phone,
+                 String email, Address address) {
+        FieldValidations.requiresNonBlank(fullName);
+        FieldValidations.requiresNonBlank(document);
+        FieldValidations.requiresNonBlank(phone);
+        FieldValidations.requiresValidEmail(email);
+        Objects.requireNonNull(address);
+        this.fullName = fullName;
+        this.document = document;
+        this.phone = phone;
+        this.email = email;
+        this.address = address;
+    }
 }
